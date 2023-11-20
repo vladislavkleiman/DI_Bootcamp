2b2956b8e138984aef2c54e1ad1ffc9e24e5532b
vladislavkleiman <kleimanvladislav@gmail.com>
2023-11-20 22:26:31 +0200

    fix sort trades

--- Final Project/Profit Diary/server/models/getTradesFromDB.model.js
+++ Final Project/Profit Diary/server/models/getTradesFromDB.model.js
@@ -1,16 +1,20 @@
 const { db } = require("../config/db.js");
 
 const getTradesFromDB = async (selectedDate) => {
-  console.log("Request received model");
+  console.log(
+    "Request received in model to fetch trades for date:",
+    selectedDate
+  );
   try {
     const formattedDate = new Date(selectedDate).toISOString().split("T")[0];
 
     const trades = await db("trades")
       .select("*")
-      .where("trade_date", "=", formattedDate); // Ensure column name matches your database schema
+      .where("trade_date", "=", formattedDate)
+      .orderBy("exectime"); // Sorting by execution time
 
     console.log("Formatted Date in Model:", formattedDate);
-    console.log("Trades:", trades);
+    console.log("Trades fetched and sorted by execution time:", trades);
     return trades;
   } catch (error) {
     console.error("Error fetching trades from the database:", error);
